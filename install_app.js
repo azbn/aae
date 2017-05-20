f211e71ed427656ec9ca07c054bf2ed95bb138b6
Alexander <alexander.zybin@gmail.com>
2017-05-20 07:02:44 +0300

    201705200702

--- install_app.js
+++ install_app.js
@@ -9,30 +9,51 @@ var app = azbn.loadApp(module);
 var argv = require('optimist').argv;
 //var spawn = require('child_process').spawn;
 
+var app_reg = azbn.loadJSON('apps');
+
 if(argv.who && argv.repo && argv.who != '' && argv.repo != '') {
 	
 	if(argv.repo.match(/^(aae.app.)/)) {
 		
+		var local_app_uid = [argv.who, argv.repo].join('/');
+		
 		azbn.echo('Ok, it is right uid: ' + argv.repo);
 		
-		var app_uid = argv.repo.replace('aae.app.', '');
-		var app_path = app_uid.split('.').join('/');
-		
-		azbn.mdl('process/child').cli('git clone https://github.com/' + argv.who + '/' + argv.repo + ' apps/' + app_path, {}, function(data){
+		if(app_reg.items[local_app_uid]) {
 			
-			//_data = JSON.parse(data);
+			azbn.echo('Ooops! App ' + local_app_uid + ' was installed');
 			
-			if(data.code == 0) {
-				
-				azbn.echo('Installed');
-				
-			} else {
-				
-				azbn.echo(data);
-				
-			}
+		} else {
 			
-		});
+			var app_uid = argv.repo.replace('aae.app.', '');
+			var app_path = app_uid.split('.').join('/');
+			
+			azbn.mdl('process/child').cli('git clone https://github.com/' + argv.who + '/' + argv.repo + ' apps/' + app_path, {}, function(data){
+				
+				//_data = JSON.parse(data);
+				
+				if(data.code == 0) {
+					
+					app_reg.items[local_app_uid] = {
+						created_at : azbn.now(),
+						who : argv.who,
+						repo : argv.repo,
+						path : 'apps/' + app_path,
+					};
+					
+					azbn.saveJSON('apps', app_reg);
+					
+					azbn.echo('Installed');
+					
+				} else {
+					
+					azbn.echo(data);
+					
+				}
+				
+			});
+			
+		}
 		
 	}
 	
